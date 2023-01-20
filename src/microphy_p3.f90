10f59131c596ccce459ff97d8a806455b9405827
Jason Milbrandt <jason.a.milbrandt@gmail.com>
2023-01-20 11:34:37 -0700

    Closing this version (now v4.5.1); preparing to merge to MASTER.
    Again, the tests in WRF and GEM are pending but expect to be near neutral;
    the commit message will be updated when done.

--- src/microphy_p3.f90
+++ src/microphy_p3.f90
@@ -20,7 +20,7 @@
 !    Melissa Cholette (melissa.cholette@ec.gc.ca)                                          !
 !__________________________________________________________________________________________!
 !                                                                                          !
-! Version:       4.5.0+dev-v4changes                                                       !
+! Version:       4.5.1                                                                     !
 ! Last updated:  2023-JAN                                                                  !
 !__________________________________________________________________________________________!
 
@@ -134,7 +134,7 @@
 
 ! Local variables and parameters:
  logical, save                  :: is_init = .false.
- character(len=1024), parameter :: version_p3                    = '4.5.0+dev-v4changes'
+ character(len=1024), parameter :: version_p3                    = '4.5.1'
  character(len=1024), parameter :: version_intended_table_1_2mom = '5.4_2momI'
  character(len=1024), parameter :: version_intended_table_1_3mom = '5.4_3momI'
  character(len=1024), parameter :: version_intended_table_2      = '5.3'
