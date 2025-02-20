43fdec2c08d6ca15f5a4d12f7e6d6910e5269682
Jason Milbrandt <jason.a.milbrandt@gmail.com>
2025-02-20 10:13:38 -0500

    Closing as v5.3.14
    This includes modifications to rime-splinting for nCat=1
    to improve freezing rain / snow in the HRDPS.

--- src/microphy_p3.f90
+++ src/microphy_p3.f90
@@ -26,7 +26,7 @@
 !    https://github.com/P3-microphysics/P3-microphysics                                    !
 !__________________________________________________________________________________________!
 !                                                                                          !
-! Version:       5.3.13 + HMnCat1FZ                                                        !
+! Version:       5.3.14                                                                    !
 ! Last updated:  2025 Feb                                                                  !
 !__________________________________________________________________________________________!
 
@@ -145,7 +145,7 @@
 
 ! Local variables and parameters:
  logical, save                  :: is_init = .false.
- character(len=1024), parameter :: version_p3                    = '5.3.13+HMnCat1FZ'
+ character(len=1024), parameter :: version_p3                    = '5.3.14'
  character(len=1024), parameter :: version_intended_table_1_2mom = '6.9-2momI'
  character(len=1024), parameter :: version_intended_table_1_3mom = '6.9-3momI'
  character(len=1024), parameter :: version_intended_table_2      = '6.2'
