714915b230cadcc2abc9d38c0568666bc6bb8423
Jason Milbrandt <jason.a.milbrandt@gmail.com>
2025-05-06 07:38:44 -0400

    Updated version number to v5.4.3 in preperation for  merging to 'main'.
    Note, there was some confusion about the version numbers for the
    dev-full3mom branch since it had, at one point, been merged to 'main'
    (formerly 'development') and subsequently advanced (to v5.4.1, etc.)
    However, due to the need to make other changes to v5.3.x, we "unmerged"
    the full3mom, keeping the dev-full3mom branch active, and made further
    changes to v5.3.x.  Now, upon merging dev-full3mom to 'main', the
    closed version will be v5.4.3. This follows from Melissa requiring to
    publish (Zenodo) a copy of the code she used for her 2025 MWR paper
    (which was technically was 'v5.4.1 + changed', which we opted to name
    'v5.4.2'.

--- src/microphy_p3.f90
+++ src/microphy_p3.f90
@@ -26,8 +26,8 @@
 !    https://github.com/P3-microphysics/P3-microphysics                                    !
 !__________________________________________________________________________________________!
 !                                                                                          !
-! Version:       5.4.3 + optimize                                                          !
-! Last updated:  2025 Apr                                                                  !
+! Version:       5.4.3                                                                     !
+! Last updated:  2025 May                                                                  !
 !__________________________________________________________________________________________!
 
  MODULE microphy_p3
@@ -146,7 +146,7 @@
 
 ! Local variables and parameters:
  logical, save                  :: is_init = .false.
- character(len=1024), parameter :: version_p3                    = '5.4.3+optimize'
+ character(len=1024), parameter :: version_p3                    = '5.4.3'
  character(len=1024), parameter :: version_intended_table_1_2mom = '6.9-2momI'
  character(len=1024), parameter :: version_intended_table_1_3mom = '6.9-3momI'
  character(len=1024), parameter :: version_intended_table_2      = '6.2'
